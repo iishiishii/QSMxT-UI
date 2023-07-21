d6e42754a3bd5d39a01345639e3f9abcce84ab55
Ashley Stewart <a.stewart.au@gmail.com>
2023-07-21 15:43:38 +1000

    Fixed hardcoded session ID in folder name

--- frontend/src/pages/Data/SubjectCard/SubjectDetailDrawer.tsx
+++ frontend/src/pages/Data/SubjectCard/SubjectDetailDrawer.tsx
@@ -34,7 +34,7 @@ const SubjectDetailDrawer: React.FC<Props> = ({ subject, run, open, setOpen, ses
 
   useEffect(() => {
     const getMetadata = async () => {
-      const metadataUrl = `${API_URL}/bids/${subject.subject}/ses-1/anat/${subject.subject}_${session}_run-${run}_echo-${selectedEcho}_part-${imageType}_MEGRE.json`
+      const metadataUrl = `${API_URL}/bids/${subject.subject}/${session}/anat/${subject.subject}_${session}_run-${run}_echo-${selectedEcho}_part-${imageType}_MEGRE.json`
       const response = await axios.get(metadataUrl);
       setImageMetadata(response.data)
     }
@@ -52,7 +52,7 @@ const SubjectDetailDrawer: React.FC<Props> = ({ subject, run, open, setOpen, ses
   }
 
   const renderImageAndDescriptions = () => {
-    const imageUrl = `${API_URL}/bids/${subject.subject}/ses-1/anat/${subject.subject}_${session}_run-${run}_echo-${selectedEcho}_part-${imageType}_MEGRE.nii`
+    const imageUrl = `${API_URL}/bids/${subject.subject}/${session}/anat/${subject.subject}_${session}_run-${run}_echo-${selectedEcho}_part-${imageType}_MEGRE.nii`
     
     const columns = [{
       title: 'Field',
